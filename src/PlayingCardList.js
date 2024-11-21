21b4f0d545fc9b538086b0c7d94757c2ddfcd18f
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-21 14:41:46 +0000

    useAxios custom hook

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,19 +1,12 @@
 import React, { useState } from "react";
-import {v1 as uuid} from "uuid";
-import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import { useAxios } from "./hooks";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from "axios";
+import {v1 as uuid} from "uuid";
 
 function useFlip(initialState = false) {
     const [isFacingUp, setIsFacingUp] = useState(initialState);
@@ -9,4 +11,14 @@ function useFlip(initialState = false) {
     return [isFacingUp, flipCard];
 }
 
-export { useFlip };
+function useAxios(url) {
+    const [data, setData] = useState([]);
+    const addData = async () => {
+        const response = await axios.get(url);
+        setData(data => [...data, { ...response.data, id: uuid() }]);
+    }
+
+    return [data, addData];
+}
+
+export { useFlip, useAxios };
